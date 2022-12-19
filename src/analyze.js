1b87fe5692d4d05458df3c3e3b72aff19cf552ce
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-12-19 15:07:59 +0000

    add comments

--- src/analyze.js
+++ src/analyze.js
@@ -3,14 +3,20 @@
 export default function analyze(array){
   if(Array.isArray(array) === false) return "Must be an array";
 
+  // sort array
   const newArray = array.sort()
 
   const object = {
-    average: array.reduce((a,b)=>a+b) / array.length,
+    // total of array and divide by length
+    average: array.reduce((a,b) => a + b) / array.length,
+    // first item of sorted array
     min: newArray[0],
+    // last item of sorted array
     max: newArray[newArray.length - 1],
+    // length of array
     length : array.length
   }
 
+  // return object
   return object
 }
